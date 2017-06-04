0f94897851daae6e6306b5230a9fcb8e55a5406e
marek-matys <marek.matys@gmail.com>
2017-06-04 22:19:24 +0200

    Another excercises

---
+++ #3-Factorial/Factorial.ps1
@@ -0,0 +1,42 @@
+﻿
+$number = Read-Host "Podaj liczbe"
+
+#$output = 1
+
+#recursive measured 
+
+function factorial_rec([int]$numer_input)
+{
+    
+    if($numer_input -gt 1) {                
+        
+        $output = $numer_input * ( factorial_rec ($numer_input - 1) )
+    }
+    return $output
+}
+
+
+
+$time_recursive =  Measure-Command{
+
+    $output_recursive =+ ( factorial_rec $number)
+
+}
+
+$time_iterative =  Measure-Command{
+
+#iterative
+
+    $output_iterative = 1
+
+    For($i=1;$i -le $number;$i++){
+        $output_iterative *= $i
+    }    
+}
+
+Write-Host "Out recursive: $output_recursive time:$($time_recursive.Ticks)"
+Write-Host "Out iterative: $output_iterative time:$($time_iterative.Ticks)"
+
+
+
+
---
+++ #5-Coin/Coin.ps1
@@ -0,0 +1,22 @@
+﻿
+$how_many_toss = Read-Host "Ile rzutow"
+
+$tails = 0 
+$heads = 0
+
+$time = Measure-Command{
+
+for($i=0;$i -lt $how_many_toss;$i++){
+    $result = Get-Random -Maximum 10 -Minimum 0
+    
+    if($result -lt 5){
+        $tails +=1
+    }else{
+        $heads += 1
+    }
+
+}
+
+}
+
+Write-Host "Number of tails:$tails, number of heads:$heads, number of throws: $how_many_toss, time: $($time.milliseconds)"
