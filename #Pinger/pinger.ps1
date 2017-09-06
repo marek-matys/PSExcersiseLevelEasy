0485477434297296aa74f4fee4374c02924d158a
marek-matys <marek.matys@gmail.com>
2017-09-06 22:32:51 +0200

    Added Exception handling

--- #Pinger/pinger.ps1
+++ #Pinger/pinger.ps1
@@ -7,7 +7,7 @@ while($true){
     $percentage =  (($success / ($success+$failure))*100)  
     Write-Progress -Activity "Testing connection ..." -PercentComplete $percentage -Status "Success:$success, Failure:$failure, Percantege:$percentage, Avg.response: $avg_time_total"
     try{          
-     $out = Test-Connection 8.8.8.8 -Count 2 -Delay 1 #-ErrorAction SilentlyContinue
+     $out = Test-Connection 8.8.8.8 -Count 2 -Delay 1 -ErrorAction Stop # Catch is only invoked on Terminating Errors
      $success++
      $avg_time += ($out.ResponseTime | Measure-Object -Average).Average
      $avg_time_total = $avg_time / ($success+$failure)
