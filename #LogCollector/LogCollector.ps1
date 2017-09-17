6ec3db24a96f46952d7024d534d8efdf94d0d418
marek-matys <marek.matys@gmail.com>
2017-09-18 01:22:33 +0200

    New files

---
+++ #LogCollector/LogCollector.ps1
@@ -0,0 +1,34 @@
+﻿$log_error_application = Get-EventLog -EntryType Error -After ( (Get-Date).AddDays(-7)) -LogName Application
+$log_warning_application = Get-EventLog -EntryType Warning -After ( (Get-Date).AddDays(-7)) -LogName Application
+
+$log_error_system = Get-EventLog -EntryType Error -After ( (Get-Date).AddDays(-7)) -LogName System 
+$log_error_system_gr = $log_error_system | Group-Object -Property source
+
+$log_warning_system = Get-EventLog -EntryType Warning -After ( (Get-Date).AddDays(-7)) -LogName System 
+
+$log_warning_system_sorted = $log_warning_system | select timeGenerated,source,instanceID,Message  | Sort-Object source,timeGenerated -Descending 
+$log_warning_system_grouped = $log_warning_system | Group-Object -Property source | select Count,Name
+
+
+$log_warning_system_grouped_html = ($log_warning_system_grouped | ConvertTo-Html -Fragment) -replace "<table>","<table class=`"warn_summary`">"
+$log_warning_system_sorted_html = ($log_warning_system_sorted | ConvertTo-Html -Fragment) -replace "<table>","<table class=`"warn_details`">"
+
+
+
+
+Summary all servers
+
+Detalis servers 
+
+
+Summary by server
+
+Details by server
+
+Top 10 error ( by source )
+
+Top 10 warnings ( by source )
+
+Top 10 Error ( by Meassage )
+
+Top 10 Warnings ( by Message )
--- #Pinger/pinger.ps1
+++ #Pinger/pinger.ps1
@@ -1,17 +1,43 @@
 ﻿$success = 1
 $failure = 0
-$avg_time = 0
-$avg_time_total = 0
+
+$ping_time = 0
+$total_time = 0
+$sample_time = 0
+
+$avg_total_time = 0
+$avg_sample_time = 0
+
+$sample_size = 100
+
+$time_array= new-object int[] $sample_size
 
 while($true){
-    $percentage =  (($success / ($success+$failure))*100)  
-    Write-Progress -Activity "Testing connection ..." -PercentComplete $percentage -Status "Success:$success, Failure:$failure, Percantege:$percentage, Avg.response: $avg_time_total"
-    try{          
-     $out = Test-Connection 8.8.8.8 -Count 2 -Delay 1 -ErrorAction Stop # Catch is only invoked on Terminating Errors
+    $percentage =  (($success / ($success+$failure))*100)
+    Write-Progress -Activity "Testing connection ..." -PercentComplete $percentage -Status ( "Success: {0} || Failure: {1} || % success: {2:f2} || Avg.response: {3:f2} || Avg.response sample: {4:f2}" -f $success,$failure,$percentage,$avg_total_time,$avg_sample_time)
+    try{
+     $out = Test-Connection 8.8.8.8 -Count 1 -Delay 1 -ErrorAction Stop # Catch is only invoked on Terminating Errors
      $success++
-     $avg_time += ($out.ResponseTime | Measure-Object -Average).Average
-     $avg_time_total = $avg_time / ($success+$failure)
+
+     $sum = ($success+$failure)
+
+     $ping_time = ($out.ResponseTime | Measure-Object -Average).Average
+     $total_time += $ping_time
+     $avg_total_time = $total_time / $sum         
+     
+     $time_array[$sum % $sample_size] = $ping_time
+     
+     if($sum -lt $sample_size){
+        $sample_time += $ping_time
+        $avg_sample_time  = $sample_time / $sum
+     }else{
+        $sample_time += $ping_time
+        $index = ($sum + 1) % $sample_size
+        $avg_sample_time = ($sample_time - $time_array[$index]) / $sample_size
+        $sample_time -= $time_array[$index]
+     }
+     
     }catch [System.Net.NetworkInformation.PingException] {
-     $failure++          
+     $failure++
     }
 }
