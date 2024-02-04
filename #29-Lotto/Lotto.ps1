5d862166670737664b9144c123be79a9b67213a2
marek-matys <marek.matys@gmail.com>
2024-02-04 23:57:11 +0100

    updates

--- #29-Lotto/Lotto.ps1
+++ #29-Lotto/Lotto.ps1
@@ -16,58 +16,39 @@ function check_input([int[]]$input_str,[int]$num_count){
     return $true
 }
 
-function generate_random_num_array([int]$numbers_count,[int]$min,[int]$max){
-    $count = 0    
-    
-    $rand_num = @()
-
-    while($count -lt $numbers_count){
-            $random = Get-Random -Minimum $min -Maximum $max
-            if($rand_num -notcontains $random){
-                $rand_num += $random
-                $count++
-            }    
-    }
-
-    return $rand_num
-
+function generate_random_num_array([int]$numbers_count){        
+    return $possibleNumbers | Get-Random -Count $numbers_count
 }
 
 $numbers_count = 6
 $minimum = 1
 $maximum = 50
+$possibleNumbers = $minimum..$maximum
 
-
-$random_numbers = generate_random_num_array $numbers_count $minimum $maximum
-
-#$random_numbers_sorted = $random_numbers | Sort-Object 
-#$random_numbers_sorted
-
-#$input = Read-Host "Podaj $numbers_count liczby oddzielone spacjami"
-
-#$input_splitted = $input -split " "
-#$input_splitted = $input_splitted | % { [int]::Parse($_) }
+$random_numbers = generate_random_num_array $numbers_count
+$random_numbers = $random_numbers | Sort-Object
 
 $number_of_drawings = 0
+
+Write-Host $random_numbers
 
 while($true){
 
     $hits_count = 0
-    $input_automatic = generate_random_num_array $numbers_count $minimum $maximum
-    #$input_automatic = $input_automatic | Sort-Object
+    $input_automatic = generate_random_num_array $numbers_count    
+    $input_automatic = $input_automatic | Sort-Object
     $number_of_drawings++
    
     #$hits_count = Compare-Object $input_automatic $random_numbers_sorted -IncludeEqual -ExcludeDifferent -PassThru | Measure-Object | Select-Object -ExpandProperty Count                                
     $hits_count = (Compare-Object $input_automatic $random_numbers -IncludeEqual -ExcludeDifferent).inputobject.Count
 
     if($hits_count -gt 4){
-        Write-Host "Liczba hitow:$hits_count Numer losowania:$number_of_drawings"
+        Write-Host "Liczba hitow:$hits_count Numer losowania:$number_of_drawings Numerki: $input_automatic"
         if($hits_count -eq 6){
-            Write-Host "Liczba hitow:$hits_count Numer losowania:$number_of_drawings"
+            Write-Host "Liczba hitow:$hits_count Numer losowania:$number_of_drawings Numerki: $input_automatic"
             break
         }  
     }
     
-
 }
 
